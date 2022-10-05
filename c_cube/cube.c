e61a40d83f1dd57044a7cffb6b94101f7b073b43
lsmucassi <lindasmucassi@gmail.com>
2022-10-05 21:31:03 +0200

    CUBE - [ calc surface ]

--- c_cube/cube.c
+++ c_cube/cube.c
@@ -1,8 +1,22 @@
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 float A, B, C;
-float cudeWith = 10;
+float cubeWidth = 10;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
+int backgroundASCIICode = ' ';
+int distanceFromCam = 60;
+float K1 = 48;
+
+float incrementSpeed = 0.6;
+float x, y, z;
+float coz;
+
+int xp, yp;
 
 float calculateX(int i, int j, int k)
 {
@@ -21,11 +35,33 @@ float calculateZ(int i, int j, int k)
     return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
+void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch)
+{
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distanceFromCam;
+
+    coz = 1/z;
+    xp = (int)(width/2 + K1 * coz * x * 2);
+    yp = (int)(height/2 + K1 * coz * y);
+
+}
+
 int main()
 {
     printf("\x1b[2J");
     while (1)
     {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4);
+
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
+        {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
+            {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+            }
+        }
     }
     return 0;
 }
